41d4e1065929b666fb5ff9ee9caf9c56f1cdc6a2
Fei <122787435@qq.com>
2025-03-26 15:02:31 +0800

    配置

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('llmserver.urls')),
 ]
+
+#添加静态资源路径配置
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
